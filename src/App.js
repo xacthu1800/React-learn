391a9c3bfcff458763dc7c06faf8e45800d75f31
2151012001 - Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-11-05 22:32:59 +0700

    sync state on URL by hook useSearchParams, useMemo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 //import './App.css';
-import { Link, Route, Routes, NavLink, Navigate  } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import AlbulmFeature from './features/Album';
 import TodoFeature from './features/Todo';
+import NotFoundComponent from './component/Notfound';
 function App() {
-  const isAuthenticated = false
   return (
     <div className="App">
       Hompage
-      <p>
-        <Link to="/todo">Todos</Link>
-      </p>
-      <p>
-        <Link to="/album">Alums</Link>
-      </p>
 
       <p>
         <NavLink to="/todo">Todos</NavLink>
@@ -22,10 +16,11 @@ function App() {
       </p>
 
       <Routes>
-        <Route path="/" Component={TodoFeature}/>
-        <Route path="/todo" element={isAuthenticated?<TodoFeature/>: <div>not authenticated</div>} />
+        <Route path="/todo/*" element={<TodoFeature />}/>
         <Route path="/album" element={<AlbulmFeature />} />
+        <Route path='*' element={<NotFoundComponent/>} />
       </Routes>
+      
     </div>
   );
 }
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,70 +1,25 @@
-import React, { useState } from 'react';
-import TodoList from './components/TodoList';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import DetailPage from './pages/Detail_Page';
+import ListPage from './pages/List_Page';
+import NotFoundComponent from '../../component/Notfound'
 
 TodoFeature.propTypes = {
     
 };
 
 function TodoFeature(props) {
-    const initTodoList = [
-        {
-            id:1,
-            title: 'Eat',
-            status: 'new'
-        },
-        {
-            id:2,
-            title: 'Sleep',
-            status: 'completed'
-        },
-        {
-            id:3,
-            title: 'Code',
-            status: 'new'
-        },
-        {
-            id:4,
-            title: 'cc',
-            status: 'completed'
-        }
-    ]
-
-    const [todoList, setTodoList]= useState(initTodoList)
-    const [filterStatus, setFilterStatus] = useState('all')
-
-    const renderTodoList = todoList.filter(todo=>filterStatus === 'all'|| filterStatus === todo.status)
-    
-    function handleTodoClick(todo, idx){
-        console.log(todo, idx)
-
-        const newTodoList = [...todoList]
-        newTodoList[idx] = {...newTodoList[idx],
-            status: newTodoList[idx].status === 'new' ? 'completed' : 'new'
-        };
-        setTodoList(newTodoList)
-    }
-
-    function handleShowCompleted(){
-        setFilterStatus('completed')
-    }
-
-    function handleShowAll(){
-        setFilterStatus('all')
-    }
-
-    function handleShowNew(){
-        setFilterStatus('new')
-    }
-
+    //const match = useMatch()
     return (
         <div>
-            <h3>Todo List</h3>       
-            <TodoList todoList={renderTodoList} onTotoClick={handleTodoClick}/>
-            <div>
-                <button onClick={handleShowCompleted}>Show Completed</button>
-                <button onClick={handleShowAll}>Show All</button>
-                <button onClick={handleShowNew}>Show New</button>
-            </div>
+            oh shiet
+
+            <Routes>
+                <Route path='/' element={<ListPage />}/>
+                <Route path='/:id' element={<DetailPage />}/>
+                <Route path='*' element={<NotFoundComponent/>} />
+            </Routes>
+
         </div>
     );
 }
