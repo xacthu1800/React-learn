87261b6b5f8062b81b03b73d4f34b7a4e3f1e48a
2151012001 - Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-11-07 15:07:53 +0700

    handle error in react hook form

--- src/component/formControl/index.jsx
+++ src/component/formControl/index.jsx
@@ -13,11 +13,29 @@ InputField.propTypes = {
 function InputField(props) {
   const { form, name, label, disabled } = props;
 
+  const { formState } = form;
+  const { errors } = formState;
+
+  /*   console.log('name:', name);
+  console.log('type of name: ', formState.defaultValues[name]);
+  console.log('formstate: ', formState); */
+
+  const hasError = !!errors[name];
+
   return (
     <Controller
       name={name}
       control={form.control}
-      render={({ field }) => <TextField {...field} fullWidth label={label} disabled={disabled} />}
+      render={({ field }) => (
+        <TextField
+          {...field}
+          fullWidth
+          label={label}
+          disabled={disabled}
+          error={hasError}
+          helperText={hasError ? errors[name]?.message : ''}
+        />
+      )}
     />
   );
 }
--- src/features/Todo/components/Todoform/index.jsx
+++ src/features/Todo/components/Todoform/index.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import InputField from '../../../../component/formControl';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 TodoForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
 function TodoForm(props) {
+  const schema = yup.object().shape({
+    title: yup.string().required('Please Enter Title'),
+  });
   const form = useForm({
     defaultValues: {
       title: '',
     },
+    resolver: yupResolver(schema),
   });
   const handleSubmit = (values) => {
     console.log('Todo form:', values);
